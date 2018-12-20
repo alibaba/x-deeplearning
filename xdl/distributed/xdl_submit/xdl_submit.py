086e08d16e8f2b3ea49becfb6396e4f7ed227756
yue.song <yue.song@alibaba-inc.com>
2018-12-20 19:15:02 +0800

    update code

--- xdl/distributed/xdl_submit/xdl_submit.py
+++ xdl/distributed/xdl_submit/xdl_submit.py
@@ -1,3 +1,6 @@
+#!/bin/env python
+#encoding=utf-8
+
 # Copyright (C) 2016-2018 Alibaba Group Holding Limited
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,10 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-#!/home/tops/bin/python
-#!/bin/env python
-#encoding=utf-8
 
 import argparse
 import json
